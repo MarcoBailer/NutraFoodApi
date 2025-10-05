caff1323dc877e1fe577a6201cec1c47db1e44fa
Marcos Bailer <marco.bailer@clickmenos.com.br>
2025-10-05 14:49:08 -0300

    Paginacao

--- Controllers/AlimentosController.cs
+++ Controllers/AlimentosController.cs
@@ -14,42 +14,122 @@ public class AlimentosController : ControllerBase
     }
 
     [HttpGet("fabricante/alimento/{alimento_fabricante}")]
-    public async Task<ActionResult<IEnumerable<Fabricante>>> BuscarAlimentosFabricantesPorNome(string alimento_fabricante)
+    public async Task<ActionResult<PaginatedResult<Fabricante>>> BuscarAlimentosFabricantesPorNome(
+    string alimento_fabricante,
+    [FromQuery] int pageNumber = 1,
+    [FromQuery] int pageSize = 10)
     {
         if (string.IsNullOrWhiteSpace(alimento_fabricante))
         {
             return BadRequest("O termo de busca não pode ser vazio.");
         }
 
-        var alimentosEncontrados = await _context.Fabricantes
-            .Where(a => a.Produto != null && a.Produto.StartsWith(alimento_fabricante))
+        var alimentosEncontrados = _context.Fabricantes.AsQueryable();
+
+        var query = alimentosEncontrados.Where(a => a.Produto != null && a.Produto.Contains(alimento_fabricante));
+
+        var totalCount = await query.CountAsync();
+        
+        var items = await query
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
             .ToListAsync();
 
-        if (!alimentosEncontrados.Any())
+        if (totalCount == 0)
         {
-            return NotFound("Nenhum alimento encontrado com o termo informado.");
+            return NotFound("Nenhum alimento encontrado com os termos informados.");
         }
 
-        return Ok(alimentosEncontrados);
+        var result = new PaginatedResult<Fabricante>
+        {
+            PageNumber = pageNumber,
+            PageSize = pageSize,
+            TotalCount = totalCount,
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize),
+            Items = items
+        };
+
+        return Ok(result);
     }
 
     [HttpGet("fastfood/alimento/{nome_fastfood}")]
-    public async Task<ActionResult<IEnumerable<FastFood>>> BuscarAlimentosFastFoodPorNome(string nome_fastfood)
+    public async Task<ActionResult<PaginatedResult<FastFood>>> BuscarAlimentosFastFoodPorNome(
+    string nome_fastfood,
+    [FromQuery] int pageNumber = 1,
+    [FromQuery] int pageSize = 10)
     {
         if (string.IsNullOrWhiteSpace(nome_fastfood))
         {
             return BadRequest("O termo de busca não pode ser vazio.");
         }
 
-        var alimentosEncontrados = await _context.FastFoods
-            .Where(a => a.Produto != null && a.Produto.StartsWith(nome_fastfood))
-            .ToListAsync();
+        var alimentosEncontrados = _context.FastFoods.AsQueryable();
 
-        if (!alimentosEncontrados.Any())
+        var query = alimentosEncontrados.Where(a => a.Produto != null && a.Produto.ToLower().Contains(nome_fastfood));
+
+        var totalCount = await query.CountAsync();
+
+        var items = await query
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+        if (totalCount == 0)
         {
-            return NotFound("Nenhum alimento encontrado com o termo informado.");
+            return NotFound("Nenhum alimento encontrado com os termos informados.");
+        }
+        var result = new PaginatedResult<FastFood>
+        {
+            PageNumber = pageNumber,
+            PageSize = pageSize,
+            TotalCount = totalCount,
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize),
+            Items = items
+        };
+
+        return Ok(result);
+    }
+
+    [HttpGet("tbca/alimento/{nome_tbca}")]
+    public async Task<ActionResult<PaginatedResult<Tbca>>> BuscarAlimentosTbcaPorNome(
+    string nome_tbca,
+    [FromQuery] int pageNumber = 1,
+    [FromQuery] int pageSize = 10)
+    {
+        if (string.IsNullOrWhiteSpace(nome_tbca))
+        {
+            return BadRequest("O termo de busca não pode ser vazio.");
         }
 
-        return Ok(alimentosEncontrados);
+        var palavrasDeBusca = nome_tbca.ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        var query = _context.Tbcas.AsQueryable();
+
+        foreach (var palavra in palavrasDeBusca)
+        {
+            query = query.Where(a => a.Nome != null && a.Nome.ToLower().Contains(palavra));
+        }
+
+        var totalCount = await query.CountAsync();
+
+        var items = await query
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+        if (totalCount == 0)
+        {
+            return NotFound("Nenhum alimento encontrado com os termos informados.");
+        }
+
+        var result = new PaginatedResult<Tbca>
+        {
+            PageNumber = pageNumber,
+            PageSize = pageSize,
+            TotalCount = totalCount,
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize),
+            Items = items
+        };
+
+        return Ok(result);
     }
 }
---
+++ Models/PaginatedResult.cs
@@ -0,0 +1,8 @@
+﻿public class PaginatedResult<T>
+{
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public int TotalPages { get; set; }
+    public List<T> Items { get; set; }
+}
